7c17dce3e7cedf506d8af9466d62264335aad578
Damian Dennis <damiandennis@Damians-MacBook-Air.local>
2025-12-08 22:30:12 +0000

    Made some major theme changes and changed some snippets

--- init.lua
+++ init.lua
@@ -25,4 +25,5 @@ vim.opt.rtp:prepend(lazypath)
 -- load plugins
 require('lazy').setup { import = 'plugins' }
 
+vim.cmd.colorscheme 'catppuccin-mocha'
 -- vim: ts=2 sts=2 sw=2 et
--- lua/plugins/catpuccin-theme.lua
+++ lua/plugins/catpuccin-theme.lua
@@ -2,15 +2,24 @@ return {
   {
     'catppuccin/nvim',
     name = 'catppuccin',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
+    -- Load the colorscheme here.
+    -- You can configure highlights by doing something like:
+    opts = {
+      transparent_background = true,
+      render_markdown = true,
+      auto_integrations = true,
+      highlight_overrides = {
+        all = function(mocha)
+          return {
+            rainbow3 = { fg = mocha.pink },
+            RenderMarkDownH3 = { fg = mocha.pink },
+            RenderMarkDownH3Bg = { fg = mocha.pink },
+            rainbow4 = { fg = mocha.mauve },
+            RenderMarkDownH4 = { fg = mocha.mauve },
+            RenderMarkDownH4Bg = { fg = mocha.mauve },
+          }
+        end,
+      },
+    },
   },
 }
