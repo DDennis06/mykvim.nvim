f3b2491e5173d7e746bd5a7d30faf05f3928e0cf
Damian Dennis <damiandennis@Damians-MacBook-Air.local>
2025-12-09 21:28:09 +0000

    Fixed some markdown things

--- lua/plugins/catpuccin-theme.lua
+++ lua/plugins/catpuccin-theme.lua
@@ -13,12 +13,21 @@ return {
           local base = mocha.base
           local darken = require('catppuccin.utils.colors').darken
           return {
+            rainbow1 = { fg = mocha.rosewater },
+            RenderMarkDownH1 = { fg = mocha.rosewater },
+            RenderMarkDownH1Bg = { bg = darken(mocha.rosewater, 0.3, base) },
+            rainbow2 = { fg = mocha.flamingo },
+            RenderMarkDownH2 = { fg = mocha.flamingo },
+            RenderMarkDownH2Bg = { bg = darken(mocha.flamingo, 0.3, base) },
             rainbow3 = { fg = mocha.pink },
             RenderMarkDownH3 = { fg = mocha.pink },
             RenderMarkDownH3Bg = { bg = darken(mocha.pink, 0.3, base) },
             rainbow4 = { fg = mocha.mauve },
             RenderMarkDownH4 = { fg = mocha.mauve },
             RenderMarkDownH4Bg = { bg = darken(mocha.mauve, 0.3, base) },
+            rainbow5 = { fg = mocha.red },
+            RenderMarkDownH5 = { fg = mocha.red },
+            RenderMarkDownH5Bg = { bg = darken(mocha.red, 0.3, base) },
           }
         end,
       },
